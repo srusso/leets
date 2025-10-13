e4e72ed2020e4a7ab05ace9b6361aa19b3d04c19
Simone Russo <simone@sr89.net>
2025-10-13 22:08:33 +0200

    Added comments

--- src/main/java/net/sr89/BuySellStockCooldown.java
+++ src/main/java/net/sr89/BuySellStockCooldown.java
@@ -2,13 +2,28 @@ package net.sr89;
 
 public class BuySellStockCooldown {
     public int maxProfit(int[] prices) {
-        int sold = 0, hold = Integer.MIN_VALUE, rest = 0;
+        int needToRest = 0, canSellOrRest = Integer.MIN_VALUE, canBuyOrRest = 0;
+
         for (int price : prices) {
-            int prvSold = sold;
-            sold = hold + price;
-            hold = Math.max(hold, rest - price);
-            rest = Math.max(rest, prvSold);
+            // needing to rest means you just sold!
+            int justSold = needToRest;
+
+            // you can get to this state only by just having sold...
+            needToRest = canSellOrRest + price; // ... so sell
+
+            // you can get to "canSellOrRest" either by...
+            canSellOrRest = Math.max(
+                    canSellOrRest, // ... keep holding (you bought earlier)
+                    canBuyOrRest - price // ... or by selling
+            );
+
+            // you can get to "canBuyOrRest" state by either...
+            canBuyOrRest = Math.max(
+                    canBuyOrRest, // ... resting
+                    justSold // ... or having just sold
+            );
         }
-        return Math.max(sold, rest);
+
+        return Math.max(needToRest, canBuyOrRest);
     }
 }
