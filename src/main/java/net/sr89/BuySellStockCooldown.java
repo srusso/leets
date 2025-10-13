db432d33bd2b283dd8249ccca4611e93d83631bd
Simone Russo <simone@sr89.net>
2025-10-13 22:29:00 +0200

    Added comments

--- src/main/java/net/sr89/BuySellStockCooldown.java
+++ src/main/java/net/sr89/BuySellStockCooldown.java
@@ -2,28 +2,39 @@ package net.sr89;
 
 public class BuySellStockCooldown {
     public int maxProfit(int[] prices) {
-        int needToRest = 0, canSellOrRest = Integer.MIN_VALUE, canBuyOrRest = 0;
+
+        // there are only three states (you have to rest, you can rest or sell, you can rest or buy)
+        // these variables represent the maximum profit at any "state" at any point in time
+
+        // you have just sold
+        int mustRest = 0,
+                // you have just held or bought
+                holdingStock = Integer.MIN_VALUE,
+                // you have just rested for the first time, or you just kept resting
+                canBuyOrRest = 0;
 
         for (int price : prices) {
-            // needing to rest means you just sold!
-            int justSold = needToRest;
+            // we only need the "mustRest" state from the previous iteration
+            int previousMustRest = mustRest;
 
-            // you can get to this state only by just having sold...
-            needToRest = canSellOrRest + price; // ... so sell
+            // now let's go to the next state, for the next iteration
 
-            // you can get to "canSellOrRest" either by...
-            canSellOrRest = Math.max(
-                    canSellOrRest, // ... keep holding (you bought earlier)
-                    canBuyOrRest - price // ... or by selling
+            // go to this state in the only possible way: by selling what you are holding
+            mustRest = holdingStock + price;
+
+            // go to "holdingStock" either by...
+            holdingStock = Math.max(
+                    holdingStock, // ... keep holding (you bought earlier)
+                    canBuyOrRest - price // ... or by buying this share
             );
 
-            // you can get to "canBuyOrRest" state by either...
+            // go to "canBuyOrRest" state by either...
             canBuyOrRest = Math.max(
-                    canBuyOrRest, // ... resting
-                    justSold // ... or having just sold
+                    canBuyOrRest, // ... keep resting
+                    previousMustRest // ... or having just rested for the first time
             );
         }
 
-        return Math.max(needToRest, canBuyOrRest);
+        return Math.max(mustRest, canBuyOrRest);
     }
 }
