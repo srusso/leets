f2e070ab890638447296bae634f53c598aff29ad
Simone Russo <simone@sr89.net>
2025-10-26 07:55:31 +0100

    Minor refactor

--- src/main/java/net/sr89/TargetSum.java
+++ src/main/java/net/sr89/TargetSum.java
@@ -4,24 +4,27 @@ package net.sr89;
  * <a href="https://leetcode.com/problems/target-sum/description/">Leetcode link</a>
  */
 public class TargetSum {
+    private static final int SIZE = 1001;
+
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] ways = new int[1001][2];
+        int[][] ways = new int[2][SIZE];
 
         int which = 0;
 
         if (nums[0] == 0) {
-            ways[nums[0]][which] += 2;
+            ways[which][nums[0]] += 2;
         } else {
-            ways[nums[0]][which]++;
+            ways[which][nums[0]]++;
         }
 
         for (int i = 1; i < nums.length; i++) {
             int n = nums[i];
-            for (int k = 0; k < ways.length; k++) {
+
+            for (int k = 0; k < SIZE; k++) {
                 if (n != 0) {
-                    ways[k][1 - which] = ways(ways, k + n, which) + ways(ways, k - n, which);
+                    ways[1 - which][k] = ways(ways, k + n, which) + ways(ways, k - n, which);
                 } else {
-                    ways[k][1 - which] = ways(ways, k, which) * 2;
+                    ways[1 - which][k] = ways(ways, k, which) * 2;
                 }
             }
 
@@ -32,9 +35,9 @@ public class TargetSum {
     }
 
     private int ways(int[][] ways, int i, int which) {
-        if (i >= ways.length) {
+        if (i >= SIZE) {
             return 0;
         }
-        return i >= 0 ? ways[i][which] : ways[-i][which];
+        return i >= 0 ? ways[which][i] : ways[which][-i];
     }
 }
