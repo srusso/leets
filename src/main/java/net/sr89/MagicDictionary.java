cb9ef19a7788d059b531e05d9b2449861626946f
Simone Russo <simone@sr89.net>
2025-10-27 18:56:26 +0100

    Magic dictionary test

---
+++ src/main/java/net/sr89/MagicDictionary.java
@@ -0,0 +1,18 @@
+package net.sr89;
+
+/**
+ * <a href="https://leetcode.com/problems/implement-magic-dictionary/">Leetcode link</a>
+ */
+public class MagicDictionary {
+    public MagicDictionary() {
+
+    }
+
+    public void buildDict(String[] dictionary) {
+
+    }
+
+    public boolean search(String searchWord) {
+        return false;
+    }
+}
---
+++ src/test/java/net/sr89/MagicDictionaryTest.java
@@ -0,0 +1,44 @@
+package net.sr89;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MagicDictionaryTest {
+    final MagicDictionary solution = new MagicDictionary();
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void runTests(Set<String> dictionary, Map<String, Boolean> searches) {
+        solution.buildDict(dictionary.toArray(new String[0]));
+
+        searches.forEach((searchTerm, expectedSearchResult) -> {
+            boolean actual = solution.search(searchTerm);
+
+            assertEquals(
+                    expectedSearchResult,
+                    actual,
+                    () -> "Expected " + expectedSearchResult + " from searching " + searchTerm + " in dictionary: " + dictionary);
+        });
+
+    }
+
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(
+                        Set.of("hello", "leetcode"),
+                        Map.of(
+                                "hello", false,
+                                "hhllo", true,
+                                "hell", false,
+                                "leetcoded", false
+                        ))
+        );
+    }
+}
