63e7ed99e4f2f1f72f222d7f3f7a600680d0b83c
Simone Russo <simone@sr89.net>
2025-11-10 15:18:49 +0100

    Solution without record class

--- src/main/java/net/sr89/MaxAreaOfIsland.java
+++ src/main/java/net/sr89/MaxAreaOfIsland.java
@@ -12,64 +12,47 @@ public class MaxAreaOfIsland {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                Point point = new Point(i, j);
-                if (isWater(grid, point)) {
+                if (isWater(grid, i, j)) {
                     continue;
                 }
 
-                maxIsland = Math.max(maxIsland, exploreIsland(grid, point));
+                maxIsland = Math.max(maxIsland, exploreIsland(grid, i, j));
             }
         }
 
         return maxIsland;
     }
 
-    private int exploreIsland(int[][] grid, Point startingPoint) {
+    private int exploreIsland(int[][] grid, int startI, int startJ) {
         int size = 0;
 
-        Queue<Point> pointsToExplore = new LinkedList<>();
-        pointsToExplore.add(startingPoint);
+        Queue<int[]> pointsToExplore = new LinkedList<>();
+        pointsToExplore.add(new int[]{startI, startJ});
 
         while (!pointsToExplore.isEmpty()) {
-            Point point = pointsToExplore.poll();
+            int[] point = pointsToExplore.poll();
+            int i = point[0];
+            int j = point[1];
 
-            if (isWithinBounds(grid, point) && !isWater(grid, point)) {
+            if (isWithinBounds(grid, i, j) && !isWater(grid, i, j)) {
                 size++;
-                grid[point.i][point.j] = 0;
+                grid[i][j] = 0;
 
-                pointsToExplore.add(point.up());
-                pointsToExplore.add(point.down());
-                pointsToExplore.add(point.left());
-                pointsToExplore.add(point.right());
+                pointsToExplore.add(new int[]{i - 1, j});
+                pointsToExplore.add(new int[]{i + 1, j});
+                pointsToExplore.add(new int[]{i, j - 1});
+                pointsToExplore.add(new int[]{i, j + 1});
             }
         }
 
         return size;
     }
 
-    private boolean isWithinBounds(int[][] grid, Point point) {
-        return point.i >= 0 && point.i < grid.length && point.j >= 0 && point.j < grid[0].length;
+    private boolean isWithinBounds(int[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length;
     }
 
-    private boolean isWater(int[][] grid, Point point) {
-        return grid[point.i][point.j] == 0;
-    }
-
-    record Point(int i, int j) {
-        public Point up() {
-            return new Point(i - 1, j);
-        }
-
-        public Point down() {
-            return new Point(i + 1, j);
-        }
-
-        public Point left() {
-            return new Point(i, j - 1);
-        }
-
-        public Point right() {
-            return new Point(i, j + 1);
-        }
+    private boolean isWater(int[][] grid, int i, int j) {
+        return grid[i][j] == 0;
     }
 }
