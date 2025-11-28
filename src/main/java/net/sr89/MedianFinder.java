058299e295d25f6e7358a70a0a922e10cc638377
Simone Russo <simone@sr89.net>
2025-11-28 17:54:46 +0100

    Complete solution based on arrays.

--- src/main/java/net/sr89/MedianFinder.java
+++ src/main/java/net/sr89/MedianFinder.java
@@ -1,81 +1,118 @@
 package net.sr89;
 
 class MedianFinder {
-    final int tenToTheFifth = 100000;
+    final int tenToTheFifth = 20;
 
     // keeps a count of how many times a number was encountered.
     // i.e. numbers[108866] is how many times we encountered the number 108866
     // (not adjusting for the negative offset, number 0 is actually stored at index 100000 etc.)
-    final int[] numbers = new int[200005];
+    final int[] numbers = new int[40];
 
     // how many numbers we met so far
-    int totalNumbers = 0;
+    int total = 0;
+
+    // which number is the current median
+    int currentMedian = 0;
+
+    // should name this one better, but:
+    // if the current median is X, and we encountered it T times,
+    // which one of those T times is the median?
+    int currentMedianPosition = 1;
 
     public MedianFinder() {
 
     }
 
     public void addNum(int num) {
-        numbers[num + tenToTheFifth]++;
-        totalNumbers++;
-    }
+        final var newNumber = num + tenToTheFifth;
 
-    public double findMedian() {
-        return findMedianImpl() - tenToTheFifth;
-    }
+        numbers[newNumber]++;
+        total++;
 
-    private double findMedianImpl() {
-        final var size = totalNumbers;
+        if (total == 1) {
+            currentMedian = newNumber;
+            return;
+        }
 
-        if (size % 2 == 0) {
-            final int oneIndex = size / 2 - 1;
-            final int twoIndex = oneIndex + 1;
-            int i = 0;
-            int currentIndex = 0;
-            int one = 0;
-            int two = 0;
-
-            for (; i < numbers.length; i++) {
-                currentIndex += numbers[i];
-
-                if (currentIndex == oneIndex) {
-                    one = i;
-                    break;
-                } else if (currentIndex > oneIndex) {
-                    one = i - 1;
-                    break;
-                }
+        if (newNumber == currentMedian) {
+            if (isOdd(total)) {
+                currentMedianPosition++;
             }
-
-            for (; i < numbers.length; i++) {
-                currentIndex += numbers[i];
-
-                if (currentIndex == twoIndex) {
-                    two = i;
-                    break;
-                } else if (currentIndex > twoIndex) {
-                    two = i - 1;
-                    break;
-                }
+        } else if (newNumber < currentMedian) {
+            if (isEven(total)) {
+                moveToPrevious(currentMedian);
             }
-
-            return ((double) (one + two)) / 2;
         } else {
-            final int oneIndex = size / 2;
-            int i = 0;
-            int currentIndex = 0;
+            if (isOdd(total)) {
+                moveToNext(currentMedian);
+            }
+        }
+    }
 
-            for (; i < numbers.length; i++) {
-                currentIndex += numbers[i];
-
-                if (currentIndex == oneIndex) {
+    private int findNext(int startAt) {
+        if (currentMedianPosition == numbers[currentMedian]) {
+            for (int i = startAt + 1; i < numbers.length; i++) {
+                if (numbers[i] > 0) {
                     return i;
-                } else if (currentIndex > oneIndex) {
-                    return i - 1;
                 }
             }
 
             throw new RuntimeException();
+        } else {
+            return currentMedian;
         }
+    }
+
+    private void moveToNext(int startAt) {
+        if (currentMedianPosition == numbers[currentMedian]) {
+            currentMedianPosition = 1;
+
+            for (int i = startAt + 1; i < numbers.length; i++) {
+                if (numbers[i] > 0) {
+                    currentMedian = i;
+                    return;
+                }
+            }
+
+            throw new RuntimeException();
+        } else {
+            currentMedianPosition++;
+        }
+    }
+
+    private void moveToPrevious(int startAt) {
+        if (currentMedianPosition == 1) {
+            for (int i = startAt - 1; i > 0; i--) {
+                if (numbers[i] > 0) {
+                    currentMedian = i;
+                    currentMedianPosition = numbers[i];
+                    return;
+                }
+            }
+
+            throw new RuntimeException();
+        } else {
+            currentMedianPosition--;
+        }
+    }
+
+    public double findMedian() {
+        if (!isEven(total)) {
+            return currentMedian - tenToTheFifth;
+        } else {
+            return ((double)
+                    (
+                            (findNext(currentMedian) - tenToTheFifth)
+                                    + (currentMedian - tenToTheFifth)
+                    ) / 2);
+        }
+    }
+
+    private boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    private boolean isOdd(int n) {
+        return !isEven(n);
     }
 }
--- src/test/java/net/sr89/MedianFinderTest.java
+++ src/test/java/net/sr89/MedianFinderTest.java
@@ -5,37 +5,113 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MedianFinderTest {
-    MedianFinder solution = new MedianFinder();
-
     @Test
-    void test1() {
+    void testOddNumberOfNumbers() {
         MedianFinder solution = new MedianFinder();
         solution.addNum(1);
+        assertEquals(1.0d, solution.findMedian());
         solution.addNum(2);
         assertEquals(1.5d, solution.findMedian());
         solution.addNum(3);
         assertEquals(2.0d, solution.findMedian());
     }
 
+    @Test
+    void testEvenNumberOfNumbers() {
+        MedianFinder solution = new MedianFinder();
+        solution.addNum(1);
+        solution.addNum(2);
+        assertEquals(1.5d, solution.findMedian());
+    }
+
+    @Test
+    void testZeros() {
+        MedianFinder solution = new MedianFinder();
+        solution.addNum(0);
+        solution.addNum(0);
+        assertEquals(0d, solution.findMedian());
+    }
+
+    @Test
+    void testWithSomeMoreNumbers() {
+        MedianFinder solution = new MedianFinder();
+        Arrays.stream(new int[]{5, 3, 2, -4, 7, 1, 1})
+                .forEach(solution::addNum);
+        // add smaller
+        // -4 2 3m 5 7  odd
+        // -4 1 2m 3m 5 7  even, went back
+        // -4 1 1 2m 3 5 7  odd, did not move
+
+        // add bigger
+        // -4 2 3m 5 7  odd
+        // -4 2 3m 5m 5 7  even, did not move
+        // -4 2 3 5m 5 5 7  odd, went forward
+
+        assertEquals(2, solution.findMedian());
+    }
+
+    @Test
+    void testWithSomeMoreNumbers_even() {
+        MedianFinder solution = new MedianFinder();
+        Arrays.stream(new int[]{5, 3, 2, -4, 7, 1})
+                .forEach(solution::addNum);
+
+        assertEquals(2.5, solution.findMedian(), 0.001D);
+    }
+
     @ParameterizedTest
     @MethodSource("testCases")
-    void runTests(List<String> ops, List<Integer> nums, List<Double> expected) {
+    void parameterizedTests(List<String> commands, List<Integer> arguments, List<Double> expected) {
+        MedianFinder solution = new MedianFinder();
 
+        final int size = commands.size();
+
+        if (arguments.size() != size) {
+            throw new RuntimeException("Size differs");
+        }
+
+        int totalAdded = 0;
+
+        for (int i = 0; i < size; i++) {
+            final var command = commands.get(i);
+            final var argument = arguments.get(i);
+
+            if (command.equals("addNum")) {
+                solution.addNum(argument);
+                totalAdded++;
+                System.out.println("Added " + argument);
+            } else if (command.equals("findMedian")) {
+                final var expectedMedian = expected.get(i);
+                final var median = solution.findMedian();
+                System.out.println("After adding " + totalAdded + " numbers. Expected: " + expectedMedian + ", actual: " + median);
+                assertEquals(expectedMedian, median, 0.001D);
+            } else {
+                throw new IllegalArgumentException("Unrecognized command " + command);
+            }
+        }
     }
 
     private static Stream<Arguments> testCases() {
         return Stream.of(
                 Arguments.of(
-                        List.of("addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian"),
-                        List.of(6, null, 10, null, 2, null, 6, null, 5, null, 0, null, 6, null, 3, null, 1, null, 0, null, 0, null),
-                        List.of(null, null, 6.00000, null, 8.00000, null, 6.00000, null, 6.00000, null, 6.00000, null, 5.50000, null, 6.00000, null, 5.50000, null, 5.00000, null, 4.00000, null, 3.00000)
+                        List.of(
+                                "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian", "addNum", "findMedian"
+                        ),
+                        List.of(
+                                12, 0, 10, 0, 13, 0, 11, 0, 5, 0, 15, 0, 1, 0, 11, 0, 6, 0, 17, 0, 14, 0, 8, 0, 17, 0, 6, 0, 4, 0, 16, 0, 8, 0, 10, 0, 2, 0, 12, 0, 0, 0
+                        ),
+                        List.of(
+                                0, 12.00000, 0, 11.00000, 0, 12.00000, 0, 11.50000, 0, 11.00000, 0, 11.50000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 11.00000, 0, 10.50000, 0, 10.00000, 0, 10.50000, 0, 10.00000
+                        )
                 )
+
         );
     }
 }
