85c3a2cdf69293630c1a0b418af1a3534e07e7a1
Simone Russo <simone@sr89.net>
2025-11-28 17:13:52 +0100

    Work in progress: Implementing different solution for the median finder

--- src/main/java/net/sr89/MedianFinder.java
+++ src/main/java/net/sr89/MedianFinder.java
@@ -1,31 +1,81 @@
 package net.sr89;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 class MedianFinder {
-    List<Integer> numbers = new ArrayList<>();
+    final int tenToTheFifth = 100000;
+
+    // keeps a count of how many times a number was encountered.
+    // i.e. numbers[108866] is how many times we encountered the number 108866
+    // (not adjusting for the negative offset, number 0 is actually stored at index 100000 etc.)
+    final int[] numbers = new int[200005];
+
+    // how many numbers we met so far
+    int totalNumbers = 0;
 
     public MedianFinder() {
-        
-    }
-    
-    public void addNum(int num) {
-        numbers.add(num);
-    }
-    
-    public double findMedian() {
-        numbers.sort(Integer::compareTo);
 
-        final var size = numbers.size();
+    }
+
+    public void addNum(int num) {
+        numbers[num + tenToTheFifth]++;
+        totalNumbers++;
+    }
+
+    public double findMedian() {
+        return findMedianImpl() - tenToTheFifth;
+    }
+
+    private double findMedianImpl() {
+        final var size = totalNumbers;
+
         if (size % 2 == 0) {
-            int one = numbers.get(size / 2 - 1);
-            int two = numbers.get(size / 2);
+            final int oneIndex = size / 2 - 1;
+            final int twoIndex = oneIndex + 1;
+            int i = 0;
+            int currentIndex = 0;
+            int one = 0;
+            int two = 0;
+
+            for (; i < numbers.length; i++) {
+                currentIndex += numbers[i];
+
+                if (currentIndex == oneIndex) {
+                    one = i;
+                    break;
+                } else if (currentIndex > oneIndex) {
+                    one = i - 1;
+                    break;
+                }
+            }
+
+            for (; i < numbers.length; i++) {
+                currentIndex += numbers[i];
+
+                if (currentIndex == twoIndex) {
+                    two = i;
+                    break;
+                } else if (currentIndex > twoIndex) {
+                    two = i - 1;
+                    break;
+                }
+            }
 
             return ((double) (one + two)) / 2;
         } else {
-            return numbers.get(size / 2);
+            final int oneIndex = size / 2;
+            int i = 0;
+            int currentIndex = 0;
+
+            for (; i < numbers.length; i++) {
+                currentIndex += numbers[i];
+
+                if (currentIndex == oneIndex) {
+                    return i;
+                } else if (currentIndex > oneIndex) {
+                    return i - 1;
+                }
+            }
+
+            throw new RuntimeException();
         }
     }
 }
