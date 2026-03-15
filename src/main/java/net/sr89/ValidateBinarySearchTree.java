b099c82cdb9ff6d824d8246c29c7869eaf5a02e2
Simone Russo <simone@sr89.net>
2026-03-15 12:31:09 +0100

    Manual queues of capacity 20.

--- src/main/java/net/sr89/ValidateBinarySearchTree.java
+++ src/main/java/net/sr89/ValidateBinarySearchTree.java
@@ -2,30 +2,28 @@ package net.sr89;
 
 import net.sr89.types.TreeNode;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 /**
  * <a href="https://leetcode.com/problems/validate-binary-search-tree/">Leetcode link</a>
  */
 public class ValidateBinarySearchTree {
+    private final TreeNode[] stack = new TreeNode[20];
+    private final Integer[] minStack = new Integer[20];
+    private final Integer[] maxStack = new Integer[20];
+
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return false;
         }
 
-        Deque<TreeNode> stack = new LinkedList<>();
-        Deque<Integer> minStack = new LinkedList<>();
-        Deque<Integer> maxStack = new LinkedList<>();
+        int stackPos = 0;
 
-        stack.push(root);
-        minStack.push(null);
-        maxStack.push(null);
+        stack[stackPos] = root;
 
-        while (!stack.isEmpty()) {
-            var currentTreeNode = stack.pop();
-            var currentAllowedMin = minStack.pop();
-            var currentAllowedMax = maxStack.pop();
+        while (stackPos >= 0) {
+            var currentTreeNode = stack[stackPos];
+            var currentAllowedMin = minStack[stackPos];
+            var currentAllowedMax = maxStack[stackPos];
+            stackPos--;
 
             boolean lessThanMin = currentAllowedMin != null && currentTreeNode.val <= currentAllowedMin;
             boolean moreThanMax = currentAllowedMax != null && currentTreeNode.val >= currentAllowedMax;
@@ -34,15 +32,17 @@ public class ValidateBinarySearchTree {
             }
 
             if (currentTreeNode.left != null) {
-                stack.push(currentTreeNode.left);
-                minStack.push(currentAllowedMin);
-                maxStack.push(currentTreeNode.val);
+                stackPos++;
+                stack[stackPos] = (currentTreeNode.left);
+                minStack[stackPos] = (currentAllowedMin);
+                maxStack[stackPos] = (currentTreeNode.val);
             }
 
             if (currentTreeNode.right != null) {
-                stack.push(currentTreeNode.right);
-                minStack.push(currentTreeNode.val);
-                maxStack.push(currentAllowedMax);
+                stackPos++;
+                stack[stackPos] = (currentTreeNode.right);
+                minStack[stackPos] = (currentTreeNode.val);
+                maxStack[stackPos] = (currentAllowedMax);
             }
         }
 
