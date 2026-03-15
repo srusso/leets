a2cb4ef3b47e67acb3827f22a8ec5f981a94c8ac
Simone Russo <simone@sr89.net>
2026-03-15 12:05:43 +0100

    Ugly solution with 3 stacks.

--- src/main/java/net/sr89/ValidateBinarySearchTree.java
+++ src/main/java/net/sr89/ValidateBinarySearchTree.java
@@ -9,57 +9,66 @@ import java.util.LinkedList;
  * <a href="https://leetcode.com/problems/validate-binary-search-tree/">Leetcode link</a>
  */
 public class ValidateBinarySearchTree {
-    private record MinMax(TreeNode node, Integer min, Integer max) {
-    }
-
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return false;
         }
 
-        Deque<MinMax> stack = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        Deque<Integer> minStack = new LinkedList<>();
+        Deque<Integer> maxStack = new LinkedList<>();
 
-        stack.push(new MinMax(root, null, null));
+        stack.push(root);
+        minStack.push(null);
+        maxStack.push(null);
 
-        MinMax lastPopped = null;
+        TreeNode lastPopped = null;
 
         while (!stack.isEmpty()) {
-            var stackTop = stack.element();
-            var currentAllowedMax = stackTop.max;
-            var currentAllowedMin = stackTop.min;
-            var currentTreeNode = stackTop.node;
-            var lastPoppedTreeNode = lastPopped == null ? null : lastPopped.node;
+            var currentTreeNode = stack.element();
+            var currentAllowedMin = minStack.element();
+            var currentAllowedMax = maxStack.element();
 
-            final boolean upFromLeft = lastPoppedTreeNode == currentTreeNode.left && currentTreeNode.left != null;
-            final boolean upFromRight = lastPoppedTreeNode == currentTreeNode.right && currentTreeNode.right != null;
+            final boolean upFromLeft = lastPopped == currentTreeNode.left && currentTreeNode.left != null;
+            final boolean upFromRight = lastPopped == currentTreeNode.right && currentTreeNode.right != null;
 
             if (currentTreeNode.left == null && currentTreeNode.right == null) {
                 // Visiting terminal node
                 lastPopped = stack.pop();
+                minStack.pop();
+                maxStack.pop();
 
-                boolean lessThanMin = currentAllowedMin == null ? false : currentTreeNode.val <= currentAllowedMin;
-                boolean moreThanMax = currentAllowedMax == null ? false : currentTreeNode.val >= currentAllowedMax;
+                boolean lessThanMin = currentAllowedMin != null && currentTreeNode.val <= currentAllowedMin;
+                boolean moreThanMax = currentAllowedMax != null && currentTreeNode.val >= currentAllowedMax;
                 if (lessThanMin || moreThanMax) {
                     return false;
                 }
             } else if (upFromLeft && currentTreeNode.right != null) {
                 // We just visited the left subtree and there is a right subtree
 
-                stack.push(new MinMax(currentTreeNode.right, currentTreeNode.val, currentAllowedMax));
+                stack.push(currentTreeNode.right);
+                minStack.push(currentTreeNode.val);
+                maxStack.push(currentAllowedMax);
             } else if (upFromLeft || upFromRight) {
                 // We just visited both available subtrees
                 lastPopped = stack.pop();
+                minStack.pop();
+                maxStack.pop();
 
-                boolean lessThanMin = currentAllowedMin == null ? false : currentTreeNode.val <= currentAllowedMin;
-                boolean moreThanMax = currentAllowedMax == null ? false : currentTreeNode.val >= currentAllowedMax;
+                boolean lessThanMin = currentAllowedMin != null && currentTreeNode.val <= currentAllowedMin;
+                boolean moreThanMax = currentAllowedMax != null && currentTreeNode.val >= currentAllowedMax;
                 if (lessThanMin || moreThanMax) {
                     return false;
                 }
             } else if (currentTreeNode.left != null) {
                 // you go left, the minimum stays the same, the maximum changes
-                stack.push(new MinMax(currentTreeNode.left, currentAllowedMin, currentTreeNode.val));
+                stack.push(currentTreeNode.left);
+                minStack.push(currentAllowedMin);
+                maxStack.push(currentTreeNode.val);
             } else {
-                stack.push(new MinMax(currentTreeNode.right, currentTreeNode.val, currentAllowedMax));
+                stack.push(currentTreeNode.right);
+                minStack.push(currentTreeNode.val);
+                maxStack.push(currentAllowedMax);
             }
         }
 
