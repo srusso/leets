a2cc88580e04411fab6b03cb931cfffb072f56d5
Simone Russo <simone@sr89.net>
2026-04-10 15:41:23 +0200

    Merge from origin

---
+++ src/main/java/net/sr89/problems/CourseSchedule2.java
@@ -0,0 +1,10 @@
+package net.sr89.problems;
+
+/**
+ * <a href="https://leetcode.com/problems/course-schedule-ii/">Leetcode</a>
+ */
+public class CourseSchedule2 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        return new int[]{};
+    }
+}
